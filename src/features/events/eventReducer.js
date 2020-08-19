bd0c062e5d2e382c22128cdc0c9e5397365eb64c
Shaho <toofani@gmail.com>
2020-08-19 17:34:17 +0430

    Add Read operation by Redux

---
+++ src/features/events/eventReducer.js
@@ -0,0 +1,39 @@
+import { sampleData } from "../../app/api/sampleData";
+import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from "./eventConstants";
+
+const initialState = {
+  events: sampleData,
+};
+
+const eventReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case CREATE_EVENT:
+      return {
+        ...state,
+        events: [...state.events, payload],
+      };
+    case UPDATE_EVENT:
+      return {
+        ...state,
+        events: [
+          ...state.events.filter((event) => {
+            return event.id !== payload.id;
+          }),
+          payload,
+        ],
+      };
+    case DELETE_EVENT:
+      return {
+        ...state,
+        events: [
+          ...state.events.filter((event) => {
+            return event.id !== payload.id;
+          }),
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default eventReducer;
--- src/index.js
+++ src/index.js
@@ -7,14 +7,19 @@ import "./app/layout/styles.css";
 import App from "./app/layout/App";
 
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
 
 const rootEl = document.getElementById("root");
+const store = configureStore();
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 }
