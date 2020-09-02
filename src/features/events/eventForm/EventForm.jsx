0d929749ab5f4c40fed39765e022064e0d0debcf
Shaho <toofani@gmail.com>
2020-09-02 17:32:43 +0430

    polish the event form with extra Formik props, isSubmiiting, dirty and isValid

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -62,35 +62,45 @@ export default function EventForm({ match, history }) {
         }}
         validationSchema={validationSchema}
       >
-        <Form className="ui form">
-          <Header sub color="teal" content="Event Details" />
-          <MyTextInput name="title" placeholder="Event title" />
-          <MySelectInput
-            name="category"
-            placeholder="Event Category"
-            options={categoryData}
-          />
-          <MyTextArea name="description" placeholder="Description" rows={3} />
-          <Header sub color="teal" content="Event Location Details" />
-          <MyTextInput name="city" placeholder="City" />
-          <MyTextInput name="venue" placeholder="Venue" />
-          <MyDateInput
-            name="date"
-            placeholderText="Date"
-            timeFormat="HH:mm"
-            showTimeSelect
-            timeCaption="time"
-            dateFormat="MMMM d, yyyy h:mm a"
-          />
-          <Button type="submit" floated="right" positive content="Submit" />
-          <Button
-            as={Link}
-            to="/events"
-            type="submit"
-            floated="right"
-            content="Cancel"
-          />
-        </Form>
+        {({ isSubmitting, dirty, isValid }) => (
+          <Form className="ui form">
+            <Header sub color="teal" content="Event Details" />
+            <MyTextInput name="title" placeholder="Event title" />
+            <MySelectInput
+              name="category"
+              placeholder="Event Category"
+              options={categoryData}
+            />
+            <MyTextArea name="description" placeholder="Description" rows={3} />
+            <Header sub color="teal" content="Event Location Details" />
+            <MyTextInput name="city" placeholder="City" />
+            <MyTextInput name="venue" placeholder="Venue" />
+            <MyDateInput
+              name="date"
+              placeholderText="Date"
+              timeFormat="HH:mm"
+              showTimeSelect
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm a"
+            />
+            <Button
+              loading={isSubmitting}
+              disabled={!isValid || !dirty || isSubmitting}
+              type="submit"
+              floated="right"
+              positive
+              content="Submit"
+            />
+            <Button
+              disabled={isSubmitting}
+              as={Link}
+              to="/events"
+              type="submit"
+              floated="right"
+              content="Cancel"
+            />
+          </Form>
+        )}
       </Formik>
     </Segment>
   );
