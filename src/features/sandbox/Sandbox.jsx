405124df81dc0e23b1504217e3faa43494b5ac82
Shaho <toofani@gmail.com>
2020-09-11 23:21:42 +0430

    Add initial Autocomplete input based on Mapbox Geolocation

---
+++ src/features/sandbox/Sandbox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MyPlaceInput from "../sandbox/Sandbox";
+
+export default function Sandbox() {
+  return (
+    <>
+      <h1>Testing 123</h1>
+      <MyPlaceInput name="search" />
+    </>
+  );
+}
